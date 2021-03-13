a5f687a803fd2fca6421681ed3f330bf4df478c3
ishandongol <ishandongol@gmail.com>
2021-03-13 09:09:36 +0000

    ADDED AUTHORIZED EMAILS

--- lib/helpers/blogs/getAllBlogs.ts
+++ lib/helpers/blogs/getAllBlogs.ts
@@ -1,10 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import {BlogsModel} from '../../models/blogs'
-import { getSession } from 'next-auth/client'
 
 export const getAllBlogs = async (req:NextApiRequest, res: NextApiResponse) => {
-    const session = await getSession({ req })
-    console.log(session)
+
     const blogs = await BlogsModel.find({})
     return res.status(200).json({ blogs });
 }
